7f9749aa58572ef5104a45b7c51d1805c7b006f7
unknown <isak.m.h.s@gmail.com>
2019-09-07 22:32:19 +0200

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/main.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <p>expense.wtf</p>
+    </div>
+  );
+}
+
+export default App;
